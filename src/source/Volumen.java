f430c758743f6d290d9032264116606cccb37434
gtello79 <gonzalotello79@gmail.com>
2021-10-03 18:56:40 -0300

    Agregando constructor de copia y funcion para analizar taza de dosis por beamlet e influencia en voxels

--- src/source/Volumen.java
+++ src/source/Volumen.java
@@ -4,22 +4,44 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Volumen {
-
-    //Tiene dimension voxels x beamlets, contiene la DDM del organo
+    // Nombre del cuerpo descrito
+    private String data;
+    // Tiene dimension voxels x beamlets, contiene la DDM del organo
     private Matrix DDM;
     // Tiene asociado el numero de voxels asociado al organo
     private int nbVoxels;
     // Tiene el numero de beamlets que influyen en el organo
     private int nb_beamlets;
 
+    //
+    private Hashtable<Integer, ArrayList<Integer>> indexDAODDM;
+
+    //
+    private Hashtable<String, Double> valueDAODDM;
 
     //Constructor del volumen asociado
     public Volumen(String data) throws FileNotFoundException {
+        this.indexDAODDM = new Hashtable();
+        this.valueDAODDM = new Hashtable();
+        this.data = data;
         this.nb_beamlets = 0;
         this.nbVoxels = 0;
 
         this.setData(data);
     }
+
+    public Volumen(Volumen v){
+
+        this.data = v.data;
+        this.DDM = new Matrix(v.getDDM());
+
+        this.nbVoxels = v.getNb_voxels();
+        this.nb_beamlets = v.getNb_beamlets();
+
+        this.indexDAODDM = v.getIndexDAODDM();
+        this.valueDAODDM = v.getValueDAODDM();
+    }
+
 
     //set data
     private void setData(String data) throws FileNotFoundException {
@@ -55,7 +77,6 @@ public class Volumen {
 
         //Total de Voxels asociados al organo/tumor
         this.nbVoxels = lines.size()-1;
-
         //Se crea la DDM inicializada en 0
         this.DDM = new Matrix(this.nbVoxels, this.nb_beamlets);
 
@@ -63,14 +84,23 @@ public class Volumen {
         for(int i = 0; i < this.nbVoxels; i++){
 
             String arrayLine = lines.get(i);
-            String [] dosesVector = arrayLine.split("\t");
+            String[] dosesVector = arrayLine.split("\t");
+            ArrayList<Integer> affectionBeamLets = new ArrayList<>();
 
             //Rellena para cada beamLet i
             for(int j = 0; j < this.nb_beamlets; j++){
                 double dosesDelivered = Double.parseDouble(dosesVector[j+1]);
                 this.DDM.setPos(i,j,dosesDelivered);
+
+                if(dosesDelivered > 0.0){
+                    affectionBeamLets.add(j);
+                }
+                String indexKey = i + "-" + j;
+                this.valueDAODDM.put(indexKey, dosesDelivered);
             }
+            this.indexDAODDM.put(i, affectionBeamLets);
         }
+
     }
 
     public int getNb_voxels(){
@@ -83,6 +113,14 @@ public class Volumen {
 
     public int getNb_beamlets(){
         return this.nb_beamlets;
+    }
+
+    public Hashtable<Integer, ArrayList<Integer>> getIndexDAODDM(){
+        return this.indexDAODDM;
+    }
+
+    public Hashtable<String, Double> getValueDAODDM() {
+        return this.valueDAODDM;
     }
     //------------------------------------------------------------------- PRINTERS -------------------------------------------------
 
