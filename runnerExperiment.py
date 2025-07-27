c672e13d263de804f3aedf528f7b7283f46301f9
gtello79 <gonzalotello79@gmail.com>
2025-07-26 22:38:03 -0400

    Experiment Runner new version

--- runnerExperiment.py
+++ runnerExperiment.py
@@ -5,7 +5,9 @@ from time import time
 from argparse import ArgumentParser
 
 CURR_DIR = os.getcwd()
-LD_LIBRARY_GUROBI_PATH = '/opt/gurobi1101/linux64/lib/gurobi.jar'
+LD_LIBRARY_GUROBI_PATH = '/opt/gurobi1103/linux64/lib/gurobi.jar'
+LB_JAVAX = 'src/com/openjfx-23.0.2_linux-x64_bin-sdk/javafx-sdk-23.0.2/lib/javafx.base.jar'
+
 folder_experiments = './ExperimentsFiles/'
 folder_finals = './FinalResults/'
 
@@ -39,14 +41,17 @@ def parse_arguments():
     parser.add_argument(
         "--c2Aperture", type=float, help="Learning constant c2 for aperture"
     )
-    parser.add_argument("--inerAperture", type=float, help="Inertia for aperture")
+    parser.add_argument("--wMaxAperture", type=float, help="Max Inertia for aperture")
+    parser.add_argument("--wMinAperture", type=float, help="Min Inertia for aperture")
+
     parser.add_argument(
         "--c1Intensity", type=float, help="Learning constant c1 for intensity"
     )
     parser.add_argument(
         "--c2Intensity", type=float, help="Learning constant c2 for intensity"
     )
-    parser.add_argument("--inerIntensity", type=float, help="Inertia for intensity")
+    parser.add_argument("--wMaxIntensity", type=float, help="Max Inertia for intensity")
+    parser.add_argument("--wMinIntensity", type=float, help="Min Inertia for intensity")
     parser.add_argument("--iter", type=int, help="N° Iteration for the swarm")
 
     args = parser.parse_args()
@@ -54,12 +59,11 @@ def parse_arguments():
 
 def main():
 
-    
     results = []
-    exp_iter = 10
+    exp_iter = 1
     nThreads = 3
-    N_EVALUATIONS = 5000
-    intensityOptimized = False
+    N_EVALUATIONS = 40000
+    intensityOptimized = True
 
     params_args = parse_arguments()
 
@@ -71,10 +75,13 @@ def main():
     size = params_args.size or 418
     c1_aper = params_args.c1Aperture or 1.8751
     c2_aper = params_args.c2Aperture or 0.2134
-    w_aper = params_args.inerAperture or 0.5774
+    wMax_aper = params_args.wMaxAperture or 0.9
+    wMin_aper = params_args.wMinAperture or 0.4
+
     c1_int = params_args.c1Intensity or 0.3158
     c2_int = params_args.c2Intensity or 1.7017
-    w_int = params_args.inerIntensity or 0.5331
+    wMax_int = params_args.wMaxIntensity or 0.9
+    wMin_int = params_args.wMinIntensity or 0.4
     iter = params_args.iter or (N_EVALUATIONS // size)
 
     if(argv.__contains__("cnAperture")):
@@ -84,28 +91,36 @@ def main():
         index = argv.index("cnIntensity")
         cn_intensity = float(argv[index+1])
 
-
-    iter = int(40000/size)
     init_time = time()
 
     #Compilacion
-    compiler_command = f"javac --class-path src:{LD_LIBRARY_GUROBI_PATH} src/com/company/Main.java "
+    compiler_command = f"javac --class-path src:{LD_LIBRARY_GUROBI_PATH}:{LB_JAVAX} src/com/company/Main.java "
     try:
         system(compiler_command)
     except SystemError as e:
         print(e)
+    
+    params = {
+        'i': instance,
+        'size': size,
+        'iter': iter,
+        'nThreads': nThreads,
+        'c1Aperture': c1_aper,
+        'c2Aperture': c2_aper,
+        'wMaxAperture': wMax_aper,
+        'wMinAperture': wMin_aper,
+        'c1Intensity': c1_int,
+        'c2Intensity': c2_int,
+        'wMaxIntensity': wMax_int,
+        'wMinIntensity': wMin_int,
+        'cnAperture': cn_aperture,
+        'cnIntensity': cn_intensity,
+        'intensityOptimized': intensityOptimized
+    }
 
-    config_params = "i "                + str(instance)         + " size "          + str(size) +\
-                    " c1Aperture "      + str(c1_aper)      + " c2Aperture "    + str(c2_aper) + \
-                    " inerAperture "    + str(w_aper)    + " c1Intensity "   + str(c1_int) +\
-                    " c2Intensity "     + str(c2_int)     + " inerIntensity " + str(w_int) + " iter " + str(iter)+ " nThreads " + str(nThreads)+ \
-                    " cnAperture "      + str(cn_aperture)      + " cnIntensity "   + str(cn_intensity)  + " "
-
-
-    if intensityOptimized:
-        config_params += " intensityOptimized " + str(intensityOptimized)
-
-    execute_command = f"java -classpath ./src:{LD_LIBRARY_GUROBI_PATH} com.company.Main " + config_params
+    config_params = " ".join([f"{key} {value}" for key, value in params.items()])
+    
+    execute_command = f"java -classpath src:{LD_LIBRARY_GUROBI_PATH}:{LB_JAVAX} com.company.Main " + config_params
 
     print("Experiments ID ", UID)
     print("PARAMS ", config_params)
